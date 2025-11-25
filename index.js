b8381b9a75192a23032f01e41d033095cfd4d9b0
ankitsinghsinghel <singhelboyankit@gmail.com>
2025-11-26 00:24:38 +0530

    update histogram buckets and restructure request counter initialization for total request counter visualization in grafana

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ const excTime= new client.Histogram({
     name: "execution_time_histogram",
     help: "Histogram for execution time of doSomeTasks",
     labelNames: ["method", "route", "code"],
-    buckets: [1, 10, 20, 100, 200, 500, 1000, 2000] 
+    buckets: [1, 10, 20, 100, 200, 500, 1000, 2000, 4000] 
+});
+
+const reqCounter = new client.Counter({
+    name: "request_counter",
+    help: "Counter for total requests received",
+    labelNames: ["method", "route", "code"]
 });
 
 app.use(resTime((req, res, time) => {
+    reqCounter.inc();
     excTime.labels({
         method:req.method,
          route:req.url,
